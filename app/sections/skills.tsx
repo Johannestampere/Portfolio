55bcf5cf7c043d1528ee190de9c6fa3982a41b20
Johannes Tampere <tamperejohannes@gmail.com>
2025-05-24 21:15:39 -0400

    skills section, landing page text

--- app/sections/skills.tsx
+++ app/sections/skills.tsx
@@ -14,6 +14,25 @@ export default function Skills(): JSX.Element {
         className="text-center max-w-3xl"
       >
         <h1 className="text-6xl font-bold mb-6">Skills</h1>
+        <div>
+          <img height="32" width="32" src="https://cdn.simpleicons.org/python/black" />
+          <img height="32" width="32" src="https://cdn.simpleicons.org/javascript/black" />
+          <img height="32" width="32" src="https://cdn.simpleicons.org/typescript/black" />
+          <img height="32" width="32" src="https://cdn.simpleicons.org/react/black" />
+          <img height="32" width="32" src="https://cdn.simpleicons.org/next.js/black" />
+          <img height="32" width="32" src="https://cdn.simpleicons.org/node.js/black" />
+          <img height="32" width="32" src="https://cdn.simpleicons.org/git/black" />
+          <img height="32" width="32" src="https://cdn.simpleicons.org/html5/black" />
+          <img height="32" width="32" src="https://cdn.simpleicons.org/mongodb/black" />
+          <img height="32" width="32" src="https://cdn.simpleicons.org/pandas/black" />
+          <img height="32" width="32" src="https://cdn.simpleicons.org/numpy/black" />
+          <img height="32" width="32" src="https://cdn.simpleicons.org/css3/black" />
+          <img height="32" width="32" src="https://cdn.simpleicons.org/tailwindcss/black" />
+          <img height="32" width="32" src="https://cdn.simpleicons.org/c/black" />
+          <img height="32" width="32" src="https://cdn.simpleicons.org/cplusplus/black" />
+          <img height="32" width="32" src="https://cdn.simpleicons.org/racket/black" />
+        </div>
+        
       </motion.div>
     </div>
   )
