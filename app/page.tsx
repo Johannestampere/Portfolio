dbcd849e79c5addfe5c387e4e65f2eea83207b32
Johannes Tampere <tamperejohannes@gmail.com>
2025-07-14 19:41:32 -0400

    Fix: unused import crashing CI

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 import { motion } from 'framer-motion'
 import Image from 'next/image'
-import { JSX, useState, useEffect } from 'react'
+import { JSX, useState } from 'react'
 
 const projects = [
   {
